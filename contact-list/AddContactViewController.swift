f798b8edd1d3f2a907d8a7721e23e246553c0b18
Erika Languirand <erika@detroitlabs.com>
2018-09-08 22:44:32 -0400

    Setting up AddContactViewController.swift & adding navigation item

---
+++ contact-list/AddContactViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddContactViewController.swift
+//  contact-list
+//
+//  Created by Erika Carlson on 9/8/18.
+//  Copyright © 2018 Erika Carlson. All rights reserved.
+//
+
+import UIKit
+
+class AddContactViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
