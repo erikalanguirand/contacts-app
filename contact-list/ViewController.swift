0ac80cb17d7c3081df120c35723b2bf654401336
Erika Languirand <erika@detroitlabs.com>
2018-09-08 21:56:13 -0400

    Initial commit

---
+++ contact-list/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  contact-list
+//
+//  Created by Erika Carlson on 9/8/18.
+//  Copyright © 2018 Erika Carlson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
