4b58aebd1d56de50b7ec5172f06a7c5104db0f01
Erika Languirand <erika@detroitlabs.com>
2018-09-08 23:31:47 -0400

    Setting up ContactDetailViewController

---
+++ contact-list/ContactDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ContactDetailViewController.swift
+//  contact-list
+//
+//  Created by Erika Carlson on 9/8/18.
+//  Copyright © 2018 Erika Carlson. All rights reserved.
+//
+
+import UIKit
+
+class ContactDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
