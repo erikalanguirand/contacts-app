62d571276fb0b2f917a464511ad1263b06473068
Erika Languirand <erika@detroitlabs.com>
2018-09-08 21:59:55 -0400

    Setting up & renaming ContactListViewController

--- contact-list/ContactListViewController.swift
+++ contact-list/ContactListViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ContactListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +18,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
 
-
+    
 }
 
